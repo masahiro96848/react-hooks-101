9b46812e879940292b4e3d3d8192331a17ccbae1
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-23 08:49:38 +0900

    タブサイズを変更

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div>
-      <p>よろしく</p>
-    </div>
-  );
+    return (
+        <div>
+            <p>よろしく</p>
+        </div>
+    );
 }
 
 export default App;
