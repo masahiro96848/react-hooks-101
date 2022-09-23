6f443a32bb5cf7cf830aecc9711f443203883540
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-23 16:48:05 +0900

    display event refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useReducer } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Event from './Event'
 import reducer from '../reducers'
 
 const App = (props) => {
@@ -54,6 +55,9 @@ const App = (props) => {
                 <th></th>
                 </tr>
             </thead>
+            <tbody>
+                {state.map((event, index) => <Event key={index} event={event} dispatch={dispatch} />)}
+            </tbody>
             </table>
         </div>
         </>
