66f6b11df81fc9fca8b3debbdc708678f7752731
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-23 16:21:42 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import reducer from '../reducers'
 
 const App = (props) => {
+    const [state, dispatch] = useReducer(reducer, [])
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
 
+    const addEvent = (e) => {
+        e.preventDefault();
+        dispatch({
+            type: 'CREATE_EVENT',
+            title,
+            body
+        })
+        setTitle('')
+        setBody('')
+    }
+    
+    
     return (
-      <>
+        <>
         <div className="container-fluid">
-          <h4>イベント作成フォーム</h4>
-          <form>
+            <h4>イベント作成フォーム</h4>
+            <form>
             <div className="form-group">
-              <label htmlFor="formEventTitle">タイトル</label>
-              <input className="form-control" id="formEventTitle" type="text" />
+                <label htmlFor="formEventTitle">タイトル</label>
+                <input className="form-control" id="formEventTitle" type="text" value={title} onChange={e => setTitle(e.target.value)} />
             </div>
             <div className="form-group">
-              <label htmlFor="formEventBody">ボディー</label>
-              <textarea
+                <label htmlFor="formEventBody">ボディー</label>
+                <textarea
                 className="form-control"
                 id="formEventBody"
                 type="text"
-              />
+                value={body} 
+                onChange={e => setBody(e.target.value)}
+                />
             </div>
 
-            <button className="btn btn-primary">イベントを作成する</button>
+            <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
             <button className="btn btn-danger">全てのイベント削除する</button>
-          </form>
+            </form>
 
-          <h4>イベント一覧</h4>
-          <table className='table table-hover'>
+            <h4>イベント一覧</h4>
+            <table className='table table-hover'>
             <thead>
-              <tr>
+                <tr>
                 <th>ID</th>
                 <th>タイトル</th>
                 <th>ボディー</th>
                 <th></th>
-              </tr>
+                </tr>
             </thead>
-          </table>
+            </table>
         </div>
-      </>
+        </>
     );
 }
 
